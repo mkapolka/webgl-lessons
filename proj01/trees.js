e1f99781b372558f30b9ea3454a26060e14533ee
Marek Kapolka <marek.kapolka@gmail.com>
2013-02-18 17:11:39 -0800

    Pushed some code around and added comments

--- proj01/trees.js
+++ proj01/trees.js
@@ -96,6 +96,7 @@ function generateSegment(root, params)
    for (var i = 0; i < numBranches; i++)
    {
       var branch = generateSegment(segment, branchParams);
+      branch.prevSegment = segment;
       segment.branches.push(branch);
    }
 
@@ -103,9 +104,21 @@ function generateSegment(root, params)
    {
       //params.segments -= 1;
       segment.nextSegment = generateSegment(segment, branchParams);
+      segment.nextSegment.prevSegment = segment;
    }
 
    return segment;
+}
+
+//Returns the root-most segment connected to the input Segment
+function getBaseSegment(segment)
+{
+   if (segment.prevSegment === null)
+   {
+      return segment;
+   } else {
+      return getBaseSegment(segment.prevSegment);
+   }
 }
 
 //Creates a new SegmentParameters object
@@ -133,4 +146,182 @@ function branchSegmentParams(params)
    sp2.segments -= 1;
 
    return sp2;
+}
+
+//Unpacks an array of vectors into an array of values.
+//This is useful because the addSegmentToRenderable method
+//expects the values in its vertexArray to be glMatrix vec3s, but
+//WebGL buffers should be filled with primative values
+function unpackArray(input)
+{
+   var output = [];
+
+   for (var child in input)
+   {
+      for (var i = 0; i < input[child].length; i++)
+      {
+         output.push(input[child][i]);
+      }
+   }
+
+   return output;
+}
+
+//Generates the vertex, element, and color array data for a tree segment
+//and its children recursively. 
+function addSegmentToRenderable(vertexArray, elementArray, colorArray,
+                                rootIndexStart, templateVertices, segment,
+                                rootMatrix, level)
+{
+   var m = mat4.create();
+   mat4.set(rootMatrix, m);
+
+   var vUp = vec3.create();
+   vUp.set([0,1,0], vUp);
+   var vFor = vec3.create();
+   vFor.set([Math.cos(segment.direction), 0, Math.sin(segment.direction)], vFor);
+   var vRot = vec3.cross(vUp, vFor);
+
+   mat4.rotate(m, segment.angle, vRot);
+   mat4.translate(m, [0, segment.segmentLength, 0]);
+
+   var startIndex = vertexArray.length;
+
+   var scaleMatrix = mat4.create();
+   mat4.identity(scaleMatrix);
+   var dist = countSegmentMaxHeight(getBaseSegment(segment)) - countSegmentMaxHeight(segment);
+   mat4.scale(scaleMatrix, [Math.pow(.6, dist), 1, Math.pow(.6, dist)]);
+   //Add vertices
+   for (var i in templateVertices)
+   {
+      //Vertex Location
+      var v = vec3.create(templateVertices[i]);
+      v = mat4.multiplyVec3(scaleMatrix, v);
+      v = mat4.multiplyVec3(m, v);
+      vertexArray.push(v);
+
+      //Color
+      var c = Math.pow(.8, level);
+      colorArray.push([c,c,c,1]);
+   }
+
+   //Elements
+   for (var i = 0; i < templateVertices.length; i++)
+   {
+      elementArray.push(rootIndexStart + i);
+      elementArray.push(startIndex + i);
+   }
+   //loop around
+   elementArray.push(rootIndexStart);
+   elementArray.push(startIndex);
+   //Degenerate
+   elementArray.push(startIndex);
+
+   //Up first
+   if (segment.nextSegment != null)
+   {
+      addSegmentToRenderable(vertexArray, elementArray, colorArray, startIndex, templateVertices, segment.nextSegment, m, level+1);
+   }
+
+   for (var branch in segment.branches)
+   {
+      //discontinuous degenerate
+      elementArray.push(rootIndexStart);
+      elementArray.push(rootIndexStart);
+      addSegmentToRenderable(vertexArray, elementArray, colorArray, rootIndexStart, templateVertices, segment.branches[branch], rootMatrix, level);
+   }
+}
+
+//Generates the pattern of vertices that will be used for each ring of the tree
+function makeTemplateVertices()
+{
+   var templateVertices = [];
+
+   for (var i = 0; i < 6; i++)
+   {
+      templateVertices.push(vec3.create([Math.cos(i * Math.PI / 3), 0, Math.sin(i * Math.PI / 3)]));
+   }
+
+   return templateVertices;
+}
+
+//Generates a Renderable tree from this segment and its children.
+function makeSegmentRenderable(segment, glContext)
+{
+   var templateVertices = makeTemplateVertices();
+
+   var vertexArray = templateVertices.slice(0);
+   var elementArray = [];
+   var colorArray = [];
+   //Initialize color array
+   for (var i = 0; i < vertexArray.length; i++)
+   {
+      colorArray.push([1,1,1,1]);
+   }
+   var rootMatrix = mat4.create();
+   mat4.identity(rootMatrix);
+
+   var renderable = new Renderable(glContext);
+   renderable.mvMatrix = rootMatrix;
+
+   addSegmentToRenderable(vertexArray, elementArray, colorArray, 0, templateVertices, segment, rootMatrix, 0);
+
+   var upColors = unpackArray(colorArray);
+   var upVerts = unpackArray(vertexArray);
+   var upElements = elementArray;
+   var upColors = unpackArray(colorArray);
+
+   renderable.setVertices(upVerts);
+   renderable.setElements(upElements);
+   renderable.setColors(upColors);
+
+   return renderable;
+}
+
+//Returns the maxiumum number of segments that follow this one
+//Before a leaf segment is found
+function countSegmentMaxHeight(segment)
+{
+   if (segment.nextSegment === null && segment.branches.length === 0)
+   {
+      return 1;
+   } else {
+      var max = 0;
+      if (segment.nextSegment != null)
+      {
+         max = countSegmentMaxHeight(segment.nextSegment) + 1;
+      } else {
+         max = 1;
+      }
+
+      for (var child in segment.branches)
+      {
+         var ch = countSegmentMaxHeight(segment.branches[child]);
+
+         if (ch > max)
+         {
+            max = ch;
+         }
+      }
+
+      return max;
+   }
+}
+
+//Counts the total number of segments below this segment in a tree.
+function countNumSegments(segment){
+   var output;
+   if (segment.nextSegment != null)
+   {
+      output = countNumSegments(segment.nextSegment) + 1;
+   } else {
+      output = 1;
+   }
+
+   for (var branch in segment.branches)
+   {
+      output += countNumSegments(branch);      
+   }
+
+   return output;
 }
